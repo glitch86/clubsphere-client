0eb7642109106e4427d32cc30886e59241df6bbf
glitch86 <mdpolokahmed825@gmail.com>
2025-12-14 12:39:51 +0600

    added update club page

--- src/Pages/EditClubs.jsx
+++ src/Pages/EditClubs.jsx
@@ -2,19 +2,21 @@ import React, { use } from "react";
 import { AuthContext } from "../Context/AuthContext";
 import { useNavigate, useParams } from "react-router";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
-import { useForm } from "react-hook-form";
-import { useQuery } from "@tanstack/react-query";
+import { useForm, useWatch } from "react-hook-form";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import bannerDummy from "../assets/bannerDummy.png";
+
 
 const EditClubs = () => {
   const { user } = use(AuthContext);
-  // console.log(user.email);
+  const queryClient = useQueryClient();
+
   const navigate = useNavigate();
   const axiosSecure = useAxiosSecure();
 
   //   fetch info
   const { id } = useParams();
-  // console.log(axiosSecure)
-  const { data: clubInfo = [], isLoading } = useQuery({
+  const { data: clubInfo = [] } = useQuery({
     queryKey: ["clubInfo", id],
     queryFn: async () => {
       const result = await axiosSecure.get(`/clubs/${id}`);
@@ -22,121 +24,151 @@ const EditClubs = () => {
     },
   });
 
-  const { register, handleSubmit } = useForm();
+
+const { mutate: updateClubInfo } = useMutation({
+    mutationFn: ({ id, data }) =>
+      axiosSecure.patch(`/clubs/${id}/update`, data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["clubs"]);
+    },
+  });
+
+  const handleUpdateClub = (data) => {
+
+    // console.log(data)
+    updateClubInfo({id, data });
+  };
+
+
+  const { register, handleSubmit,control } = useForm();
+  const imgURL = useWatch({ control, name: "bannerImage" });
 
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen">
-      <h2 className="text-2xl font-semibold text-center mb-2">Editing {clubInfo.clubName}</h2>
-      <form
-        onSubmit={handleSubmit()}
-        className="card w-full max-w-lg p-6 space-y-4 md:grid grid-cols-2 gap-4"
-      >
-        {/* clubname */}
-        <div>
-          <label className="label">
-            <span className="label-text">Title</span>
-          </label>
-          <input
-            type="text"
-            defaultValue={clubInfo.clubName}
-            {...register("name", { required: "Name is required" })}
-            className="input input-bordered w-full"
-            placeholder="Club Name"
-          />
-        </div>
-        {/* category */}
-        <div>
-          <label className="label font-medium">Catergory</label>
-          <select
-            defaultValue={clubInfo.category}
-            {...register("category", { required: "Category is required" })}
-            className="select w-full rounded-full focus:border-0 focus:outline-gray-200"
-          >
-            <option value="" disabled>
-              Select Catergory
-            </option>
-            <option value="Photography">Photography</option>
-            <option value="Tech">Tech</option>
-            <option value="Books">Books</option>
-            <option value="Art">Art</option>
-            <option value="Gaming">Gaming</option>
-          </select>
-        </div>
-        {/* Location */}
-        <div>
-          <label className="label">
-            <span className="label-text">Location</span>
-          </label>
-          <input
-            type="text"
-            defaultValue={clubInfo.location}
-            {...register("location", { required: "Location is required" })}
-            className="input input-bordered w-full"
-            placeholder="Location"
-          />
-        </div>
+    <div>
+      <div>
+        <h1 className="heading text-center">Editng {clubInfo.clubName}</h1>
+        <div className="hero  min-h-screen">
+          <div className="hero-content  flex-col lg:flex-row md:gap-40">
+            <img className="rounded-2xl" src={imgURL || bannerDummy} alt="" />
+            <div>
+              <form
+                onSubmit={handleSubmit(handleUpdateClub)}
+                className="card w-full max-w-lg p-6 space-y-4 md:grid grid-cols-2 gap-4"
+              >
+                {/* clubname */}
+                <div>
+                  <label className="label">
+                    <span className="label-text">Title</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue={clubInfo.clubName}
+                    {...register("name", { required: "Name is required" })}
+                    className="input input-bordered w-full"
+                    placeholder="Club Name"
+                  />
+                </div>
+                {/* category */}
+                <div>
+                  <label className="label font-medium">Category</label>
+                  <select
+                    defaultValue={clubInfo.category}
+                    {...register("category", {
+                      required: "Category is required",
+                    })}
+                    className="select w-full rounded-full focus:border-0 focus:outline-gray-200"
+                  >
+                    <option value="" disabled>
+                      Select Category
+                    </option>
+                    <option value="Photography">Photography</option>
+                    <option value="Tech">Tech</option>
+                    <option value="Books">Books</option>
+                    <option value="Art">Art</option>
+                    <option value="Gaming">Gaming</option>
+                  </select>
+                </div>
+                {/* Location */}
+                <div>
+                  <label className="label">
+                    <span className="label-text">Location</span>
+                  </label>
+                  <input
+                    type="text"
+                    defaultValue={clubInfo.location}
+                    {...register("location", {
+                      required: "Location is required",
+                    })}
+                    className="input input-bordered w-full"
+                    placeholder="Location"
+                  />
+                </div>
 
-        {/* fee */}
-        <div>
-          <label className="label">
-            <span className="label-text">Membership Fee</span>
-          </label>
-          <input
-            type="number"
-            defaultValue={clubInfo.membershipFee}
+                {/* fee */}
+                <div>
+                  <label className="label">
+                    <span className="label-text">Membership Fee</span>
+                  </label>
+                  <input
+                    type="number"
+                    defaultValue={clubInfo.membershipFee}
+                    {...register("fee", { required: "Fee is required" })}
+                    className="input input-bordered w-full"
+                    placeholder="starts from $0"
+                  />
+                </div>
 
-            {...register("fee", { required: "Fee is required" })}
-            className="input input-bordered w-full"
-            placeholder="starts from $0"
-          />
-        </div>
+                {/* banner image */}
+                <div>
+                  <label className="label">
+                    <span className="label-text">Banner URL</span>
+                  </label>
+                  <input
+                    type="url"
+                    defaultValue={clubInfo.bannerImage}
+                    {...register("bannerImage")}
+                    className="input input-bordered w-full"
+                    placeholder="https://example.com/poster.jpg"
+                  />
+                </div>
+                <div>
+                  <label className="label">
+                    <span className="label-text">Created By</span>
+                  </label>
+                  <input
+                    type="email"
+                    {...register("addedBy")}
+                    defaultValue={user.email}
+                    readOnly
+                    className="input input-bordered w-full"
+                  />
+                </div>
 
-        {/* banner image */}
-        <div>
-          <label className="label">
-            <span className="label-text">Banner URL</span>
-          </label>
-          <input
-            type="url"
-            defaultValue={clubInfo.bannerImage}
-            {...register("url")}
-            className="input input-bordered w-full"
-            placeholder="https://example.com/poster.jpg"
-          />
-        </div>
-        <div>
-          <label className="label">
-            <span className="label-text">Created By</span>
-          </label>
-          <input
-            type="email"
-            {...register("addedBy")}
-            defaultValue={user.email}
-            readOnly
-            className="input input-bordered w-full"
-          />
-        </div>
+                {/* description */}
+                <div className="col-span-2">
+                  <label className="label">
+                    <span className="label-text">Description</span>
+                  </label>
+                  <textarea
+                    defaultValue={clubInfo.description}
+                    {...register("description")}
+                    className="textarea textarea-bordered w-full"
+                    rows="3"
+                    placeholder="A rogue pilot must fight against..."
+                  ></textarea>
+                </div>
 
-        {/* description */}
-        <div className="col-span-2">
-          <label className="label">
-            <span className="label-text">Description</span>
-          </label>
-          <textarea
-          defaultValue={clubInfo.description}
-            {...register("description")}
-            className="textarea textarea-bordered w-full"
-            rows="3"
-            placeholder="A rogue pilot must fight against..."
-          ></textarea>
+                <div className="pt-4 col-span-2">
+                  <button type="submit" className="btn btn-primary w-full">
+                    Commit Changes
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
+      </div>
 
-        <div className="pt-4 col-span-2">
-          <button type="submit" className="btn btn-primary w-full">
-           Commit Changes
-          </button>
-        </div>
-      </form>
     </div>
   );
 };
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -24,8 +24,6 @@ const Register = () => {
     formState: { errors },
   } = useForm();
 
-
-
   // sign up with email and pass
   const handleSignUp = (data) => {
     const displayName = data?.name;
@@ -83,112 +81,115 @@ const Register = () => {
   // console.log(imgURL);
 
   return (
-    <div className="h-[10000px]">
-      <div className="hidden md:block text-left">
-        <h1 className="heading">Register</h1>
-      </div>
+    <div>
+      <h1 className="heading text-center">
+        Register
+      </h1>
+      <div className="hero  min-h-screen">
+        <div className="hero-content  flex-col lg:flex-row md:gap-40">
+          <img className="rounded-2xl" src={imgURL || dummy} alt="" />
+          <div>
+            <form
+              onSubmit={handleSubmit(handleSignUp)}
+              className="fieldset rounded-box w-xs p-4 "
+            >
+              <div className="flex flex-col md:flex-row gap-6">
+                {/* name */}
+                <div>
+                  <label className="">Name</label>
+                  <input
+                    type="text"
+                    className="input"
+                    {...register("name", { required: "Name is required" })}
+                    placeholder="Name"
+                  />
 
-      <div className="flex flex-col gap-8 md:flex-row justify-center items-center h-screen">
-        <div className="md:block ">
-          <img src={imgURL || dummy} alt="" />
-        </div>
-        <div>
-          <div className="text-left md:hidden">
-            <h1 className="heading">Register</h1>
-          </div>
-          <form
-            onSubmit={handleSubmit(handleSignUp)}
-            className="fieldset rounded-box w-xs p-4  bg-base-200"
-          >
-            {/* name */}
-            <label className="">Name</label>
-            <input
-              type="text"
-              className="input"
-              {...register("name", { required: "Name is required" })}
-              placeholder="Name"
-            />
+                  {errors.name && (
+                    <span className="text-red-500 text-sm">
+                      {errors.name?.message}
+                    </span>
+                  )}
+                </div>
+                {/* photo url */}
 
-            {errors.name && (
-              <span className="text-red-500 text-sm">
-                {errors.name?.message}
-              </span>
-            )}
-            {/* photo url */}
-            <label className="">Profile URL</label>
-            <input
-              type="url"
-              className="input"
-              {...register("photoURL")}
-              placeholder="Photo URL"
-            />
-            {/* email */}
-            <label className="">Email</label>
-            <input
-              type="email"
-              className="input"
-              {...register("email", { required: "Email in required" })}
-              placeholder="Email"
-            />
-            {errors.email && (
-              <span className="text-red-500 text-sm">
-                {errors.email?.message}
-              </span>
-            )}
-            {/* password */}
-            <div className="relative">
-              <label className="">Password</label>
+                <div>
+                  <label className="">Profile URL</label>
+                  <input
+                    type="url"
+                    className="input"
+                    {...register("photoURL")}
+                    placeholder="Photo URL"
+                  />
+                </div>
+              </div>
+              {/* email */}
+              <label className="">Email</label>
               <input
-                type={showPass ? "text" : "password"}
+                type="email"
                 className="input"
-                {...register("password", {
-                  required: "Password in required",
-                  pattern: {
-                    value: /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/,
-                    message:
-                      "Password must have at least one uppercase, one lowercase letter and minimum 6 characters",
-                  },
-                })}
-                placeholder="Password"
+                {...register("email", { required: "Email in required" })}
+                placeholder="Email"
               />
-              {errors.password && (
+              {errors.email && (
                 <span className="text-red-500 text-sm">
-                  {errors.password?.message}
+                  {errors.email?.message}
                 </span>
               )}
+              {/* password */}
+              <div className="relative">
+                <label className="">Password</label>
+                <input
+                  type={showPass ? "text" : "password"}
+                  className="input"
+                  {...register("password", {
+                    required: "Password in required",
+                    pattern: {
+                      value: /^(?=.*[a-z])(?=.*[A-Z]).{6,}$/,
+                      message:
+                        "Password must have at least one uppercase, one lowercase letter and minimum 6 characters",
+                    },
+                  })}
+                  placeholder="Password"
+                />
+                {errors.password && (
+                  <span className="text-red-500 text-sm">
+                    {errors.password?.message}
+                  </span>
+                )}
 
-              <span
-                onClick={() => setShowPass(!showPass)}
-                className="absolute right-2 top-8 cursor-pointer z-50"
+                <span
+                  onClick={() => setShowPass(!showPass)}
+                  className="absolute right-2 top-8 cursor-pointer z-50"
+                >
+                  {showPass ? <FaEye size={14} /> : <IoEyeOff size={14} />}
+                </span>
+              </div>
+
+              {/* <span className="hover:underline cursor-pointer">Forget Password</span> */}
+
+              <button className="btn btn-primary mt-4">Sign Up</button>
+
+              <p className="text-center text-sm mt-3">
+                Already have an account?{" "}
+                <Link to="/login" className="hover:underline">
+                  Login
+                </Link>
+              </p>
+              <div className="divider">OR</div>
+              <button
+                type="button"
+                onClick={handleGoogleSignin}
+                className="flex items-center justify-center gap-3 bg-white text-gray-800 px-5 py-2 rounded-lg w-full font-semibold hover:bg-gray-100 transition-colors cursor-pointer my-2"
               >
-                {showPass ? <FaEye size={14} /> : <IoEyeOff size={14} />}
-              </span>
-            </div>
-
-            {/* <span className="hover:underline cursor-pointer">Forget Password</span> */}
-
-            <button className="btn btn-primary mt-4">Sign Up</button>
-
-            <p className="text-center text-sm mt-3">
-              Already have an account?{" "}
-              <Link to="/login" className="hover:underline">
-                Login
-              </Link>
-            </p>
-            <div className="divider">OR</div>
-            <button
-              type="button"
-              onClick={handleGoogleSignin}
-              className="flex items-center justify-center gap-3 bg-white text-gray-800 px-5 py-2 rounded-lg w-full font-semibold hover:bg-gray-100 transition-colors cursor-pointer my-2"
-            >
-              <img
-                src="https://www.svgrepo.com/show/475656/google-color.svg"
-                alt="google"
-                className="w-5 h-5"
-              />
-              Continue with Google
-            </button>
-          </form>
+                <img
+                  src="https://www.svgrepo.com/show/475656/google-color.svg"
+                  alt="google"
+                  className="w-5 h-5"
+                />
+                Continue with Google
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </div>
